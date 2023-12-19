655de7346c62446f848ed5facab2a5f41f565e7a
Lars Benedikt Kaesberg <l.kaesberg@stud.uni-goettingen.de>
2023-12-19 15:52:43 +0100

    feat: added padding to nav screens

--- composeApp/src/commonMain/kotlin/navigation/presentation/NavigationScreen.kt
+++ composeApp/src/commonMain/kotlin/navigation/presentation/NavigationScreen.kt
@@ -3,14 +3,15 @@ package navigation.presentation
 import App
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
@@ -25,53 +26,9 @@ fun NavigationScreen(
     selectedItem: NavigationEnum,
     onEvent: (NavigationEvent) -> Unit
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        NavHost(
-            modifier = Modifier.fillMaxSize(),
-            navigator = state.navigator,
-            navTransition = NavTransition(),
-            initialRoute = NavigationEnum.entries.first().path
-        ) {
-
-            NavigationEnum.entries.forEach {
-                when (it) {
-                    NavigationEnum.HOME -> scene(
-                        route = it.path,
-                        navTransition = NavTransition()
-                    ) {
-                    }
-
-                    NavigationEnum.FRIENDS -> scene(
-                        route = it.path,
-                        navTransition = NavTransition()
-                    ) {
-                        val viewModel = getViewModel(
-                            key = "friend-list-screen",
-                            factory = viewModelFactory {
-                                FriendListViewModel()
-                            }
-                        )
-                        val stateFriend by viewModel.state.collectAsState()
-                        FriendListScreen(
-                            state = stateFriend,
-                            newFriend = viewModel.newFriend,
-                            onEvent = viewModel::onEvent
-                        )
-                    }
-
-                    NavigationEnum.LOGIN -> scene(
-                        route = it.path,
-                        navTransition = NavTransition()
-                    ) {
-                        App()
-                    }
-                }
-            }
-        }
+    Scaffold(bottomBar = {
         if (selectedItem.navBar) {
-            NavigationBar(
-                modifier = Modifier.align(Alignment.BottomCenter)
-            ) {
+            NavigationBar {
                 NavigationEnum.entries.filter { it.navItem }.forEach { item ->
                     NavigationBarItem(
                         selected = selectedItem == item,
@@ -88,6 +45,52 @@ fun NavigationScreen(
                         },
                         label = { Text(text = item.label) }
                     )
+                }
+            }
+        }
+    }) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+
+            NavHost(
+                modifier = Modifier.fillMaxSize(),
+                navigator = state.navigator,
+                navTransition = NavTransition(),
+                initialRoute = NavigationEnum.entries.first().path
+            ) {
+
+                NavigationEnum.entries.forEach {
+                    when (it) {
+                        NavigationEnum.HOME -> scene(
+                            route = it.path,
+                            navTransition = NavTransition()
+                        ) {
+                        }
+
+                        NavigationEnum.FRIENDS -> scene(
+                            route = it.path,
+                            navTransition = NavTransition()
+                        ) {
+                            val viewModel = getViewModel(
+                                key = "friend-list-screen",
+                                factory = viewModelFactory {
+                                    FriendListViewModel()
+                                }
+                            )
+                            val stateFriend by viewModel.state.collectAsState()
+                            FriendListScreen(
+                                state = stateFriend,
+                                newFriend = viewModel.newFriend,
+                                onEvent = viewModel::onEvent
+                            )
+                        }
+
+                        NavigationEnum.LOGIN -> scene(
+                            route = it.path,
+                            navTransition = NavTransition()
+                        ) {
+                            App()
+                        }
+                    }
                 }
             }
         }
