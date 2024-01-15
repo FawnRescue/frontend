b613c63eb7ec31ea4259e2d73bf89d10c32ea5de
Lars Benedikt Kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-01-15 15:34:09 +0100

    feat: added scan button

--- composeApp/src/commonMain/kotlin/hangar/presentation/discover/DiscoverEvent.kt
+++ composeApp/src/commonMain/kotlin/hangar/presentation/discover/DiscoverEvent.kt
@@ -1,6 +1,5 @@
 package hangar.presentation.discover
 
 sealed interface DiscoverEvent {
-    object SampleEvent : DiscoverEvent
-    data class SampleEventWithData(val data: Unit) : DiscoverEvent
+    object OnScanDevices : DiscoverEvent
 }
--- composeApp/src/commonMain/kotlin/hangar/presentation/discover/DiscoverScreen.kt
+++ composeApp/src/commonMain/kotlin/hangar/presentation/discover/DiscoverScreen.kt
@@ -1,10 +1,21 @@
 package hangar.presentation.discover
 
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun DiscoverScreen(onEvent: (DiscoverEvent) -> Unit, state: DiscoverState) {
+    Column {
+        Button(onClick = { onEvent(DiscoverEvent.OnScanDevices) }) {
+            Text("Scan for Drones")
+        }
+        state.discoveredDevices.map {
+            Text(it)
+        }
+    }
 
 }
 
