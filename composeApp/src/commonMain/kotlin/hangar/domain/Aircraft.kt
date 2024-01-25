3f59d0d4ea10f3397a37af5cb56a987ff979fa0b
Lars Benedikt Kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-01-25 17:38:03 +0100

    feat: added hangar connect to database

--- composeApp/src/commonMain/kotlin/hangar/domain/Aircraft.kt
+++ composeApp/src/commonMain/kotlin/hangar/domain/Aircraft.kt
@@ -1,8 +1,14 @@
 package hangar.domain
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Aircraft(
     val id: String,
     val owner: String,
+    val name: String,
     val created_at: String,
-    val description: String
+    val description: String?,
+    val key: String,
+    val token: String
 )
--- composeApp/src/commonMain/kotlin/hangar/presentation/HangarScreen.kt
+++ composeApp/src/commonMain/kotlin/hangar/presentation/HangarScreen.kt
@@ -1,17 +1,48 @@
 package hangar.presentation
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.PersonAdd
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import friends.presentation.FriendListEvent
+import io.github.jan.supabase.compose.auth.ui.email.OutlinedEmailField
+import io.github.jan.supabase.compose.auth.ui.password.OutlinedPasswordField
+import io.github.jan.supabase.compose.auth.ui.password.PasswordRule
+import io.github.jan.supabase.compose.auth.ui.password.rememberPasswordRuleList
+import login.presentation.LoginEvent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HangarScreen(onEvent: (HangarEvent) -> Unit, state: HangarState) {
     Scaffold(floatingActionButton = {
@@ -27,6 +58,62 @@ fun HangarScreen(onEvent: (HangarEvent) -> Unit, state: HangarState) {
             )
         }
     }) {
+        if (state.aircrafts == null) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.width(64.dp),
+                    color = MaterialTheme.colorScheme.secondary,
+                    trackColor = MaterialTheme.colorScheme.surfaceVariant,
+                )
+            }
+            return@Scaffold
+        }
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
+        ) {
+            item {
+                Text(
+                    text = "My aircrafts (${state.aircrafts.size})",
+                    modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            items(state.aircrafts) { aircraft ->
+                Row {
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = { onEvent(HangarEvent.OnSelectAircraft(aircraft)) }) {
+
+                        Text(modifier = Modifier.padding(16.dp), text = aircraft.name)
+                    }
+                }
+            }
+        }
+        if (state.selectedAircraft != null) {
+            Dialog(onDismissRequest = { onEvent(HangarEvent.OnDismissDialog) }) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10))
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(state.selectedAircraft.name)
+                    }
+                }
+            }
+        }
 
     }
 }
