a86260cfbed2b6cefb528b6599d9689180cec55e
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-01-28 00:12:24 +0100

    feat: added BatteryIndicator

--- composeApp/src/commonMain/kotlin/hangar/domain/DroneStatus.kt
+++ composeApp/src/commonMain/kotlin/hangar/domain/DroneStatus.kt
@@ -3,4 +3,4 @@ package hangar.domain
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class DroneStatus(val state: DroneState, val battery: Int?, val location: String?)
+data class DroneStatus(val state: DroneState, val battery: Float?, val location: String?)
