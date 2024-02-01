a68f0349f915a863e359b6c1265a40591a1d421e
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-02-01 11:51:53 +0100

    remove: AircraftSecret.kt

--- composeApp/src/commonMain/kotlin/hangar/domain/AircraftSecret.kt
+++
@@ -1,9 +0,0 @@
-package hangar.domain
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AircraftSecret(val owner: String, val token: String, val key: String)
-
-@Serializable
-data class InsertableAircraftSecret(val owner: String? = null, val token: String, val key: String)
