26fab11fea978c2fefb8381f4c59266cdc36cac0
Luca Francis <lucajoshua.francis@stud.uni-goettingen.de>
2024-05-27 14:39:27 +0200

    feat(hangar): allow for aircraft properties change

--- composeApp/src/commonMain/kotlin/hangar/presentation/HangarState.kt
+++ composeApp/src/commonMain/kotlin/hangar/presentation/HangarState.kt
@@ -6,6 +6,8 @@ import repository.domain.Aircraft
 data class HangarState(
     val aircrafts: List<Aircraft>?,
     val selectedAircraft: Aircraft?,
+    val editableAircraft: Aircraft?,
     val aircraftStatus: AircraftStatus?,
     val loading: Boolean = true,
+    val editable: Boolean = false
 )
